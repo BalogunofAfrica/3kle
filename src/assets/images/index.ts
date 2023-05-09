ed4c248eebbe2fb5e7542d7fba17fcbd27305a5f
balogunofafrica <balogunakanbi.k@gmail.com>
2023-05-09 17:04:36 +0100

    chore: add static asset

---
+++ src/assets/images/index.ts
@@ -0,0 +1,8 @@
+export { default as bar_chart } from "./bar-chart.png";
+export { default as coins_amico } from "./coins-amico.png";
+export { default as card_bg } from "./card-bg.png";
+export { default as pie_chart } from "./pie-chart.png";
+export { default as percent55 } from "./55.png";
+export { default as percent10 } from "./10.png";
+export { default as percent5 } from "./5.png";
+export { default as percent30 } from "./30.png";
